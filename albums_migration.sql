a939cb0adac6f073ec410b294534bdacd9dcc7c7
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-10-09 16:40:31 -0500

    .

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,9 +3,9 @@ USE codeup_test_db;
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
-    artist VARCHAR(100),
+    artist VARCHAR(250),
     name VARCHAR(100),
-    release_date DATE,
-    sales DECIMAL(10,2),
+    release_date int(4),
+    sales FLOAT(5,2),
     genre VARCHAR(100)
 );
