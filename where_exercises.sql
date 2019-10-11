e17cb0e6d54a788b76be0e9d6e070d04aaa1476a
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-10-11 11:30:57 -0500

    finish where exercise

---
+++ where_exercises.sql
@@ -0,0 +1,38 @@
+USE employees;
+
+# SELECT * FROM employees
+# WHERE gender = 'M'
+#     AND first_name = 'Irena'
+#     OR gender = 'M'
+#     AND first_name = 'Vidya'
+#     OR gender = 'M'
+#     AND first_name = 'Maya';
+
+
+# SELECT * FROM employees
+# WHERE last_name like 'E%'
+#     OR last_name like '%E';
+
+
+# SELECT * FROM employees
+# WHERE last_name like 'E%'
+#    and last_name like '%E';
+
+
+SELECT * FROM employees
+WHERE hire_date LIKE '199%'
+    AND birth_date like '%-12-25';
+
+
+# SELECT * FROM employees
+# WHERE birth_date like '%-12-25';
+
+
+
+SELECT * FROM employees
+WHERE last_name like 'q%'
+    AND last_name NOT like 'qu%'
+    OR last_name like  '%q%'
+    AND last_name NOT like '%qu%'
+    OR last_name like  '%q'
+    AND last_name NOT like '%qu';
